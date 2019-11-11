438d4f2646de0247a54785e6a61d2bbb40335a55
Tarun Cholkar <tarun.cholkar@appsassoc.com>
2019-11-11 16:32:58 +0530

    Python1

---
+++ hello/views.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello Django!")
+
+def hello_there(request, name):
+    
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+        )
